b549b1b6072caf2589befa2b91150cb41a928f89
其徽 <guangcheng.song@gmail.com>
2014-12-03 18:20:11 +0800

    合并pbl_authenticatable到pbl_service_client

---
+++ lib/devise/models/pbl_authenticatable.rb
@@ -0,0 +1,24 @@
+module Devise
+  module Models
+    module PblAuthenticatable
+      extend ActiveSupport::Concern
+
+      module ClassMethods
+        def serialize_from_session(key, salt)
+          resource = Pbl::Models::Users::User.find(key)
+          resource if salt == resource.authenticatable_salt
+        end
+
+        def serialize_into_session(resource)
+          [resource.username, resource.authenticatable_salt]
+        end
+      end
+
+      def authenticatable_salt
+        password_digest[0,29] if password_digest
+      end
+    end
+  end
+end
+
+Devise.add_module :pbl_authenticatable, :controller => :sessions
